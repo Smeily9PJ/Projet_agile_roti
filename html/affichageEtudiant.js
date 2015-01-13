21052f82079c0b76181afa259573f3089468a696
julieP <juliepelissou@laposte.net>
2015-01-13 17:06:22 +0100

    correction du css et de la page de l'étudiant

--- html/affichageEtudiant.js
+++ html/affichageEtudiant.js
@@ -7,7 +7,7 @@ function chrono(){
 	if (secondes == 0){
 		if (minutes == 0){
 			newChrono = "Maintenant !";
-			document.getElementById("bouton").style.display = "block";
+			document.getElementById("affichageEtudiant_voter").style.display = "block";
 		}else{
 			secondes=59; minutes--;
 			newChrono = minutes +" minutes et "+ secondes +"secondes.";
