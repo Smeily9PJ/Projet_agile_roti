12ea80f692f8450ea95f53f14ae4a259202d7cdb
Maxime <max_rodez@msn.com>
2015-01-21 10:47:29 +0100

    CSS accueil modifié CSS etudiant en cours

--- html/voteEtudiant.js
+++ html/voteEtudiant.js
@@ -2,5 +2,11 @@ function miseAJourValeurRange(){
 	var valeurAffiche = document.getElementById("voteEtudiant_valeurVote");
 	var valeurAAffiche = document.getElementById("voteEtudiant_valeurRange");
 	valeurAffiche.textContent = valeurAAffiche.value;
+	
+	function miseAJourValeurRange(vote) {
+
+		  document.querySelector('#voteEtudiant_valeurVote').value = vote;
+
+	}
 }
 
