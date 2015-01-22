4f832b432b7674748aab6d232512942c5dd37140
julieP <juliepelissou@laposte.net>
2015-01-22 18:18:46 +0100

    démarrage du tracé de la courbe sur affichageProf.jsp avec du js dans affichageProf.js

--- javascript/affichageProf.js
+++ javascript/affichageProf.js
@@ -30,3 +30,36 @@ function afficherLaBonneHumeur (){
 		default: document.getElementById("smiley").src='images/humeurs/sonne.png'; break;
 	}
 }
+
+
+/*window.onload = function()
+{
+    var canvas = document.getElementById('affichageProf_canvas');
+        if(!canvas)
+        {
+            alert("Impossible de récupérer le canvas");
+            return;
+        }
+
+    var context = canvas.getContext('2d');
+        if(!context)
+        {
+            alert("Impossible de récupérer le context du canvas");
+            return;
+        }
+        
+        var xBase = 0 ; //a récuperer
+        var yBase = 300; //a récuperer aussi
+        var yDest = 100;
+        var xDest = 100;
+        var taille = 600;
+
+        context.beginPath();//On démarre un nouveau tracé
+        context.moveTo(xBase, yBase);//On se déplace au coin inférieur gauche
+        context.lineTo(0, taille);
+        context.lineTo(0, taille);
+        context.lineTo(-taille, 0);
+        context.lineTo(0, -taille);
+        context.stroke();//On trace seulement les lignes.
+        context.closePath();
+}*/
