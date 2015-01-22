9857baea7f296c88faea827968a99f0016be1058
julieP <juliepelissou@laposte.net>
2015-01-22 17:30:53 +0100

    contenant de humeur.js ajouté à affichageProf.js et suppression de humeur.js

--- javascript/affichageProf.js
+++ javascript/affichageProf.js
@@ -18,3 +18,15 @@ function chrono(){
 	chrono.textContent = newChrono;
 	setTimeout('chrono()',1000) //la fonction est relancée
 }
+
+
+
+
+var numeroDeLHumeurAAfficher = 2;
+function afficherLaBonneHumeur (){
+	switch(numeroDeLHumeurAAfficher){
+		case 1: document.getElementById("smiley").src='images/humeurs/sonne.png'; break;
+		case 2: document.getElementById("smiley").src='images/humeurs/dodo.png'; break;
+		default: document.getElementById("smiley").src='images/humeurs/sonne.png'; break;
+	}
+}
